939b4ec85d699bdfb9292424d40cb50e8f876771
Nhat <50206162+NhatLg@users.noreply.github.com>
2023-03-12 21:46:20 +0100

    implement save function that works with add button

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 import tkinter as tk
+from tkinter import messagebox
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_password():
+    website = input_website.get()
+    email = input_email.get()
+    password = input_pass.get()
+    saved_entry = f"{website} | {email} | {password} \n"
+    if not (website and email and password):
+        messagebox.showwarning(title="Empty field", message="Please fill in all fields")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"The info that you entered for the above website is: "
+                                                              f"\nEmail: {email}"
+                                                              f"\nPassword: {password}"
+                                                              f"\nAre you sure you want to save?")
+        if is_ok:
+            with open('data.txt', 'a') as f:
+                f.write(saved_entry)
+                input_pass.delete(0, 'end')
+                input_website.delete(0, 'end')
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = tk.Tk()
@@ -16,7 +36,7 @@ canvas.create_image(100, 100, image=img_lock)
 canvas.grid(row=0, column=1)
 
 # Button
-btn_add = tk.Button(text="Add", highlightthickness=0, width=44)
+btn_add = tk.Button(text="Add", highlightthickness=0, width=44, command=save_password)
 btn_gen_pass = tk.Button(text="Generate Password", highlightthickness=0)
 
 btn_add.grid(row=4, column=1, columnspan=2)
@@ -34,7 +54,9 @@ l_password.grid(row=3, column=0)
 
 # Entries
 input_website = tk.Entry(width=52)
+input_website.focus()
 input_email = tk.Entry(width=52)
+input_email.insert(0, "example@gmail.com")
 input_pass = tk.Entry(width=34)
 
 input_website.grid(row=1, column=1, columnspan=2)
